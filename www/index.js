3c702a0e708237f47189a67e830b8cb2ea84e50d
hedianjie <1163486362@qq.com>
2019-07-01 06:53:45 +0800

    s

--- www/index.js
+++ www/index.js
@@ -1,5 +1,3 @@
-
-
 /**
  * 盒子方法
  */
@@ -16,8 +14,8 @@ class BoxGather {
         this.len = this.opt.renderList.length;
         // 角度
         this.deg = 360 / this.len;
-        // 旋转次数
-        this.time = 0;
+        // 转了多少圈
+        this.rotate = 0;
 
         this.render();
     }
@@ -45,8 +43,8 @@ class BoxGather {
         const fragment = document.createDocumentFragment();
         for(let i = 0; i < this.len; i++) {
             const element = document.createElement('span');
-            element.className = "box-list ";
-            element.style.transform = `rotate(${this.deg * i}deg)`;
+            element.className = "box-list";
+            element.style.transform = `rotate(${-1 * this.deg * i}deg)`;
             element.innerHTML = this.opt.renderList[i];
             fragment.appendChild(element);
         }
@@ -54,23 +52,32 @@ class BoxGather {
         this.element.appendChild(fragment);
     }
 
-    go () {
-        const name = this.element.className;
-        this.time++;
-        this.element.className = name + " animated faster bounce";
-        this.element.style.transform = `rotate(${this.deg * this.time}deg)`;
-        window.setTimeout(() =>{
-            this.element.className = name;
-        }, 500)
+    go (time) {
+        if(time === null || time === undefined) return;
+        this.element.style.transform = `rotate(${this.deg * time + this.rotate * 360}deg)`;
+        console.log(time, this.len)
+        if(time === this.len - 1) {
+            this.rotate++;
+            console.log("true");
+        }
     }
 }
 
+const getCurrentTime = () => {
+    var myDate = new Date();
+    var year = myDate.getFullYear();
+    var month = myDate.getMonth() + 1;
+    var day = myDate.getDate()
+    var hour = myDate.getHours();
+    var minute = myDate.getMinutes();
+    var second = myDate.getSeconds();
 
-
-// const renderMinute = () => {
-//     const fragment = document.createDocumentFragment();
-//     for(let i = 0; i <= 60; i++) {
-//         const 
-
-//     }
-// }
+    return {
+        year,
+        month,
+        day,
+        hour,
+        minute,
+        second
+    }
+}
